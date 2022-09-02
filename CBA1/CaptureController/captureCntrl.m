e2a0183953d1409b0efff698402918f64888086b
Jrool <86136263+Jrool@users.noreply.github.com>
2022-09-02 17:55:55 +1000

    Modified Capture Controller

--- CBA1/CaptureController/captureCntrl.m
+++ CBA1/CaptureController/captureCntrl.m
@@ -1,30 +1,50 @@
 clc;
 clear all
 METR7203_PendulumPlant
-%% State Space Feedback
+% %% State Space Feedback
+m = 0.1580;
+lcg = 0.2698;
+rh = 0.37;
+J1 = 0.1314;
+Jz_bar = 0.0131;
+g = 9.81;
+c = 0.1;
+k = 0.2;
+Eo = 0;
+Id = 0.0148;
+dtheta1 = 400 * (2*pi/60);
 %Use the linearised model to develop state space Feedback
 C = eye(4);
 D = zeros(4,1);
 orig_plant = ss(A,B,C, D);
 %Develop a statespace 
-P = [-1, -2, -3, -4];
+P = [-1, -2, -3, -4] *1.5;
 K1 = place(A,B, P);
 
-plant1 = feedback(orig_plant, K1);
+plant1 = feedback(orig_plant, K1); 
 
 P =  2 * [-1, -2, -3, -4];
 K2 = place(A,B, P);
 plant2 = feedback(orig_plant, K2);
 
 Q = [
-    1, 0, 0, 0; %Penalize theta_3 error
-    0, 1, 0, 0; %Penalize dTheta_3 error
-    0, 0, 5, 0; %Penalize theta_4 eror
+    0.1, 0, 0, 0; %Penalize theta_3 error
+    0, 0.1, 0, 0; %Penalize dTheta_3 error
+    0, 0, 1, 0; %Penalize theta_4 eror
     0, 0, 0, 1]; %Penalize dtheta_4 error
 
 
-R = 0.01; %Penalise the amount of torque applied to the controller
+R = 1; %Penalise the amount of torque applied to the controller
 
 [K3, S, e] = lqr(A,B, Q, R);
 sys3 = feedback(orig_plant, K3);
 initial(sys3,[0,0,0.1,0])
+
+Q2 = [
+    0.1, 0, 0, 0; %Penalize theta_3 error
+    0, 0.1, 0, 0; %Penalize dTheta_3 error
+    0, 0, 0.1, 0; %Penalize theta_4 eror
+    0, 0, 0, 0.1]; %Penalize dtheta_4 error
+R2 = 1;
+
+[K4, S,e] = lqr(A,B,Q2,R2);
